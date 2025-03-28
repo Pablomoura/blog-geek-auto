e82e8fa60506f21fe5cf32c7f3d38afe2d0e2721
Pablo <pablo.h.moura@gmail.com>
2025-03-28 19:46:56 -0300

    atualização

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9111051074987830"
           crossOrigin="anonymous"
-          strategy="afterInteractive"
+          strategy="beforeInteractive"
         />
         {/* Google Analytics */}
         <Script
